3992bf581d6065d289cc268eaf3e307ae87be24c
Piers Karsenbarg <piers@pulumi.com>
2023-02-14 17:13:15 +0000

    First commit

---
+++ index.ts
@@ -0,0 +1,67 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as crypto from "crypto";
+
+const role = new aws.iam.Role("role", {
+    assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal(aws.iam.Principals.LambdaPrincipal),
+    managedPolicyArns: [aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole]
+});
+
+const fn = new aws.lambda.CallbackFunction("fn", {
+    callback: async () => {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({"message": "Hello, World!"})
+        }
+    },
+    role: role,
+})
+
+const api = new aws.apigateway.RestApi("api");
+
+const resource = new aws.apigateway.Resource("resource", {
+    parentId: api.rootResourceId,
+    restApi: api.id,
+    pathPart: "categories"
+})
+
+const method = new aws.apigateway.Method("root-GET-method", {
+    httpMethod: "GET",
+    resourceId: resource.id,
+    restApi: api.id,
+    authorization: "NONE",
+});
+
+const integration = new aws.apigateway.Integration("integration", {
+    restApi: api.id,
+    resourceId: resource.id,
+    httpMethod: method.httpMethod,
+    integrationHttpMethod: "POST",
+    type: "AWS_PROXY",
+    uri: fn.invokeArn
+});
+
+const pulumiResources = [resource, integration, method];
+
+pulumi.all(pulumiResources)
+    .apply(values => pulumi.jsonStringify(values))
+    .apply(json => console.log(json))
+
+
+const deployment = new aws.apigateway.Deployment("test-deployment", {
+    restApi: api.id,
+    triggers: {
+        redeployment: pulumi.all(pulumiResources)
+            .apply(values => pulumi.jsonStringify(values))
+            .apply(json => {
+                console.log(json)
+                return crypto.createHash('sha1').update(json).digest('hex')
+            }),
+    }
+    },
+    {
+        dependsOn: pulumiResources,
+        retainOnDelete: true
+    }
+)
+
